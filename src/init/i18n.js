97be098592f8444fcb006998469d80a2f01a07bd
Jesús Martínez Saugar <jesus.martinez.saugar@gmail.com>
2021-01-12 20:53:38 +0100

    fix: issue in languages dropdown label when browser language not supported

--- src/init/i18n.js
+++ src/init/i18n.js
@@ -3,7 +3,7 @@ import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-import { defaultLanguageCode, i18nNamespaces } from '#constants';
+import { defaultLanguageCode, languageCodes, i18nNamespaces } from '#constants';
 
 // @todo fix issue in dropdown menu when default language not supported
 export default () => i18n
@@ -11,11 +11,10 @@ export default () => i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    // debug @todo
+    supportedLngs : Object.values(languageCodes),
     fallbackLng   : defaultLanguageCode,
     ns            : i18nNamespaces,
     defaultNS     : i18nNamespaces[0],
-    // keyseparator @todo
     interpolation : {
       escapeValue : false, // Not needed for react as it escapes by default
     },
