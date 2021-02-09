290955ca3829d396bf338d2a4c5ac81357f2c112
Jesús Martínez Saugar <jesus.martinez.saugar@gmail.com>
2021-02-09 20:10:49 +0100

    fix: HeadMeta component failing tests

--- src/components/HeadMeta/HeadMeta.test.js
+++ src/components/HeadMeta/HeadMeta.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { wait } from '@testing-library/react';
+import { HelmetProvider } from 'react-helmet-async';
 
 import { render } from '#devTools/tests/utils';
 import routes from '#routes';
@@ -8,16 +9,22 @@ import HeadMeta from './HeadMeta';
 
 describe('Component : HeadMeta', () => {
   test('Renders correct meta in home page', async () => {
-    render(<HeadMeta route={routes.home} />);
+    render(
+      <HelmetProvider>
+        <HeadMeta route={routes.home} />
+      </HelmetProvider>,
+    );
 
     await wait(() => expect(document.title).toBe('home:head.title'));
   });
 
   test('Renders correct meta in board page', async () => {
     render(
-      <HeadMeta
-        route={routes.board}
-      />,
+      <HelmetProvider>
+        <HeadMeta
+          route={routes.board}
+        />
+      </HelmetProvider>,
     );
 
     await wait(() => expect(document.title).toBe('board:head.title'));
